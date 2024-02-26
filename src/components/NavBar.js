4bfe2b5c086c0402f0307eb2f5c98d034e6c6055
JimRohn <genuaf@lsbu.ac.uk>
2024-02-26 11:53:52 +0000

    second commit . I fixed the NavBar Icons

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -14,15 +14,27 @@ import ListItemText from '@mui/material/ListItemText';
 import InboxIcon from '@mui/icons-material/MoveToInbox';
 import MailIcon from '@mui/icons-material/Mail';
 import SearchAppBar from './SearchAppBar';
+import PeopleIcon from '@mui/icons-material/People';
+import WorkIcon from '@mui/icons-material/Work';
+import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
+import SchoolIcon from '@mui/icons-material/School';
+import HomeIcon from '@mui/icons-material/Home';  
 const drawerWidth = 240;
+
+const drawerItems = [
+  { text: "Home", icon: <HomeIcon /> , path: "/home"},
+  { text: "People", icon: <PeopleIcon />, path: "/people"},
+  { text: "Matters", icon: <WorkIcon />, path: "/matters"},
+  { text: "Events", icon: <CalendarMonthIcon />, path: "/events"},
+  { text: "Learning", icon: <SchoolIcon />, path: "/learning"},
+];
+
 
 export default function PermanentDrawerLeft() {
   return (
     <Box sx={{ display: 'flex' }}>
       <CssBaseline />
  
-  
-      
       <Drawer
         sx={{
           width: drawerWidth,
@@ -38,31 +50,18 @@ export default function PermanentDrawerLeft() {
         <Toolbar />
         <Divider />
         <List>
-          {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-            <ListItem key={text} disablePadding>
+          {drawerItems.map((item) => (
+            <ListItem key={item.text} disablePadding>
               <ListItemButton>
-                <ListItemIcon>
-                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                </ListItemIcon>
-                <ListItemText primary={text} />
-              </ListItemButton>
-            </ListItem>
-          ))}
-        </List>
-        <Divider />
-        <List>
-          {['All mail', 'Trash', 'Spam'].map((text, index) => (
-            <ListItem key={text} disablePadding>
-              <ListItemButton>
-                <ListItemIcon>
-                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                </ListItemIcon>
-                <ListItemText primary={text} />
+                <ListItemIcon>{item.icon}</ListItemIcon>
+                <ListItemText primary={item.text} />
               </ListItemButton>
             </ListItem>
           ))}
         </List>
       </Drawer>
+      
+    
       <Box
         component="main"
         sx={{ flexGrow: 1, bgcolor: 'background.default', p: 3 }}
